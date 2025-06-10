3f34be04b2dd8427f55ed612f4a457245533c9ab
AstroGoat <lucashuguet31@gmail.com>
2025-06-10 09:08:59 +0200

    add more graphs + real inertia values

--- graph.py
+++ graph.py
@@ -20,8 +20,8 @@ def read_simple(file):
         reader = csv.DictReader(f, delimiter=",")
         for i, row in enumerate(reader):
             time.append(float(i * 1/fps))
-            x.append(float(row["X"]))
-            y.append(float(row["Y"]))
+            x.append(float(row["X1"]))
+            y.append(float(row["Y1"]))
 
     return (time, x, y)
 
@@ -104,22 +104,22 @@ def cartesian_to_polar(x, y):
 # ----------------------------------------------------------------
 
 # ---------------- PLOT REAL DOUBLE PENDULUM ---------------------
-simulation_time = 15 # in seconds
-steps = fps * simulation_time
-time, x1, y1, x2, y2 = read_double(sys.argv[1])
+# simulation_time = 15 # in seconds
+# steps = fps * simulation_time
+# time, x1, y1, x2, y2 = read_double(sys.argv[1])
 
-theta1 = cartesian_to_polar(x1, y1)
-theta2 = cartesian_to_polar(
-    [x2i - x1i for x1i, x2i in zip(x1, x2)],
-    [y2i - y1i for y1i, y2i in zip(y1, y2)]
-)
+# theta1 = cartesian_to_polar(x1, y1)
+# theta2 = cartesian_to_polar(
+#     [x2i - x1i for x1i, x2i in zip(x1, x2)],
+#     [y2i - y1i for y1i, y2i in zip(y1, y2)]
+# )
 
-plt.scatter(time[:steps], theta1[:steps], label="θ1")
-plt.scatter(time[:steps], theta2[:steps], label="θ2")
+# plt.scatter(time[:steps], theta1[:steps], label="θ1")
+# plt.scatter(time[:steps], theta2[:steps], label="θ2")
 
-plt.xlabel("temps (en s)")
-plt.ylabel("θ (en rad)")
-plt.title("Double pendule réel")
+# plt.xlabel("temps (en s)")
+# plt.ylabel("θ (en rad)")
+# plt.title("Double pendule réel")
 # ----------------------------------------------------------------
 
 # ----------- PLOT REAL DOUBLE PENDULUM PHASE DIAGRAM ------------
@@ -237,7 +237,181 @@ plt.title("Double pendule réel")
 # plt.title("Influence des conditions initiales sur le pendule double réel")
 # ----------------------------------------------------------------
 
+# --------------------- PLOT SIMPLE PENDULUM ---------------------
+# simulation_time = 3 # in seconds
+# steps = fps * simulation_time
+# time, x, y = read_simple(sys.argv[1])
+
+# theta = cartesian_to_polar(x, y)
+# dtheta = [0] + [(theta[i+1] - theta[i]) * fps for i in range(len(theta) -1)]
+
+# plt.scatter(time[:steps], theta[:steps])
+
+# plt.xlabel("temps (en s)")
+# plt.ylabel("θ (en rad)")
+# plt.title("Pendule simple réel")
+# ----------------------------------------------------------------
+
+# -------------- PLOT DOUBLE PENDULUM PHASE DIAGRAM --------------
+# simulation_time = 15 # in seconds
+# steps = fps * simulation_time
+
+# theta1_0 = np.pi/2
+# theta2_0 = 0
+
+# pendulum = Pendulum(
+#     pendulum_type="double",
+#     theta1=theta1_0, theta2=theta2_0,
+#     l1=0.5, l2=0.5,
+#     m1=0.3, m2=0.3
+# )
+
+# time = []
+# theta1 = []
+# dtheta1 = []
+# theta2 = []
+# dtheta2 = []
+# for j in range(steps):
+#     time.append(j * 1/fps)
+#     theta1.append(pendulum.theta1)
+#     dtheta1.append(pendulum.dtheta1)
+#     theta2.append(pendulum.theta2)
+#     dtheta2.append(pendulum.dtheta2)
+#     pendulum.update(1/fps)
+
+# plt.plot(theta1, dtheta1, label="θ1")
+# plt.plot(theta2, dtheta2, label="θ2")
+
+# plt.xlabel("θ (en rad)")
+# plt.ylabel("dθ/dt (en rad/s)")
+# plt.title("Diagramme de phase d'un pendule double")
+# ----------------------------------------------------------------
+
+# -------------- PLOT DOUBLE PENDULUM PHASE DIAGRAM --------------
+# simulation_time = 15 # in seconds
+# steps = fps * simulation_time
+
+# theta1_0 = np.pi/2
+# theta2_0 = 0
+
+# pendulum = Pendulum(
+#     pendulum_type="double",
+#     theta1=theta1_0, theta2=theta2_0,
+#     l1=0.5, l2=0.5,
+#     m1=0.3, m2=0.3
+# )
+
+# time = []
+# theta1 = []
+# dtheta1 = []
+# theta2 = []
+# dtheta2 = []
+# for j in range(steps):
+#     time.append(j * 1/fps)
+#     theta1.append(pendulum.theta1)
+#     dtheta1.append(pendulum.dtheta1)
+#     theta2.append(pendulum.theta2)
+#     dtheta2.append(pendulum.dtheta2)
+#     pendulum.update(1/fps)
+
+# plt.plot(theta1, dtheta1, label="θ1")
+# plt.plot(theta2, dtheta2, label="θ2")
+
+# plt.xlabel("θ (en rad)")
+# plt.ylabel("dθ/dt (en rad/s)")
+# plt.title("Diagramme de phase d'un pendule double")
+# ----------------------------------------------------------------
+
+# ------------------ PLOT PHYSIC COMPARAISON ---------------------
+# simulation_time = 15 # in seconds
+# steps = fps * simulation_time
+
+# time, x1, y1, x2, y2 = read_double(sys.argv[1])
+# ttheta1 = cartesian_to_polar(x1, y1)
+# ttheta2 = cartesian_to_polar(
+#     [x2i - x1i for x1i, x2i in zip(x1, x2)],
+#     [y2i - y1i for y1i, y2i in zip(y1, y2)]
+# )
+
+# theta1_0 = ttheta1[0]
+# theta2_0 = ttheta2[0]
+
+# pendulum = Pendulum(
+#     pendulum_type="double", model="physic",
+#     theta1=theta1_0, theta2=theta2_0,
+#     l1=0.5, l2=0.5,
+#     d1=0.275, d2=0.202,
+#     m1=0.3, m2=0.3,
+#     i1=0.0291, i2=0.0144
+# )
+
+# time = []
+# theta1 = []
+# dtheta1 = []
+# theta2 = []
+# dtheta2 = []
+# for j in range(steps):
+#     time.append(j * 1/fps)
+#     theta1.append(pendulum.theta1)
+#     dtheta1.append(pendulum.dtheta1)
+#     theta2.append(pendulum.theta2)
+#     dtheta2.append(pendulum.dtheta2)
+#     pendulum.update(1/fps)
+
+# plt.plot(time, theta1[:steps], label="simulation")
+# plt.scatter(time, ttheta1[:steps], label="réel")
+
+# plt.xlabel("temps (en s)")
+# plt.ylabel("θ1 (en rad)")
+# plt.title("Comparaison pendule double réel/simulé")
+# ----------------------------------------------------------------
+
+# ------------------ PLOT CLASSIC COMPARAISON --------------------
+# simulation_time = 15 # in seconds
+# steps = fps * simulation_time
+
+# time, x1, y1, x2, y2 = read_double(sys.argv[1])
+# ttheta1 = cartesian_to_polar(x1, y1)
+# ttheta2 = cartesian_to_polar(
+#     [x2i - x1i for x1i, x2i in zip(x1, x2)],
+#     [y2i - y1i for y1i, y2i in zip(y1, y2)]
+# )
+
+# theta1_0 = ttheta1[0]
+# theta2_0 = ttheta2[0]
+
+# pendulum = Pendulum(
+#     pendulum_type="double",
+#     theta1=theta1_0, theta2=theta2_0,
+#     l1=0.5, l2=0.5,
+#     m1=0.3, m2=0.3
+# )
+
+# time = []
+# theta1 = []
+# dtheta1 = []
+# theta2 = []
+# dtheta2 = []
+# for j in range(steps):
+#     time.append(j * 1/fps)
+#     theta1.append(pendulum.theta1)
+#     dtheta1.append(pendulum.dtheta1)
+#     theta2.append(pendulum.theta2)
+#     dtheta2.append(pendulum.dtheta2)
+#     pendulum.update(1/fps)
+
+# plt.plot(time, theta1[:steps], label="simulation")
+# plt.scatter(time, ttheta1[:steps], label="réel")
+
+# plt.xlabel("temps (en s)")
+# plt.ylabel("θ1 (en rad)")
+# plt.title("Comparaison pendule double réel/simulé")
+# ----------------------------------------------------------------
+
 # ------------------ DEFAULT PYPLOT CONFIG -----------------------
+# plt.gca().xaxis.set_major_locator(MultipleLocator(0.1))
+# plt.gca().yaxis.set_major_locator(MultipleLocator(0.1))
+
 plt.gca().xaxis.set_major_locator(MultipleLocator(1))
 
 plt.gca().yaxis.set_major_locator(MultipleLocator(np.pi / 4))
@@ -251,5 +425,5 @@ plt.ylim(-np.pi, np.pi)
 # plt.xlim(-np.pi, np.pi)
 # ----------------------------------------------------------------
 
-plt.legend()
+# plt.legend()
 plt.show()
